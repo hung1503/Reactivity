926a50c4820ef6bdfdcf29fd982bb69219dc4cd8
hung1503 <hungnguyen150300@gmail.com>
2025-03-05 17:52:45 +0200

    Adding update profile in server

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -12,5 +12,11 @@ namespace API.Controllers
             return HandleResult(await Mediator.Send(new Details.Query{Username = username})); 
         }
         
+        [HttpPut]
+        public async Task<IActionResult> EditProfile(string username, Profile profile)
+        {
+            profile.Username = username;
+            return HandleResult(await Mediator.Send(new Edit.Command{DisplayName = profile.DisplayName, Bio = profile.Bio}));
+        }
     }
 }
